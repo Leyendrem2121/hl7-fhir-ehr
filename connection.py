c193eac5b56cd1bd69aea76a10f70dd46bf707f5
Leyendrem2121 <brayanynicolas07@gmail.com>
2025-05-27 12:25:52 -0500

    Update connection.py
    Correccion de collection name

--- connection.py
+++ connection.py
@@ -1,25 +1,10 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-# URI de conexión a MongoDB Atlas - ¡Usando la que tú proporcionaste!
-MONGODB_URI = "mongodb+srv://brayanruiz:Max2005@cluster0.xevyoo8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-DB_NAME = "LIS" # El nombre de la base de datos que queremos usar
 
-def get_db(): # Renombrado a 'get_db' para que coincida con el import en app.py
-    """
-    Establece y devuelve una conexión a la base de datos MongoDB.
-    """
-    try:
-        # Crea un nuevo cliente y una nueva conexión al servidor
-        client = MongoClient(MONGODB_URI, server_api=ServerApi('1'))
-        
-        # Haz ping a tu implementación para confirmar una conexión exitosa
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-        
-        # Devuelve la base de datos específica (en este caso, 'LIS')
-        # Esto es lo que app.py espera (el objeto 'db' para hacer db.appointments)
-        return client[DB_NAME] 
-    except Exception as e:
-        print(f"Error al conectar a MongoDB: {e}")
-        return None
+def connect_to_mongodb(db_name, collection_name):
+    uri = "mongodb+srv://brayanruiz:Max2005@cluster0.xevyoo8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    db = SamplePatientService
+    collection = patients
+    return collection
