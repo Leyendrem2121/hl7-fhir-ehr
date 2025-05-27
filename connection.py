43606ed90f70ab68fc3caf7e734b2687a64037cd
Leyendrem2121 <brayanynicolas07@gmail.com>
2025-05-27 13:13:00 -0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -2,16 +2,15 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from pymongo.errors import ConnectionFailure
 
-
-def connect_to_mongodb(db_name, collection_name):
+def get_db(db_name, collection_name):
     uri = "mongodb+srv://brayanruiz:Max2005@cluster0.xevyoo8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     try:
         client = MongoClient(uri, server_api=ServerApi('1'))
         # Probar la conexión (ping)
         client.admin.command('ping')
         print("Conexión a MongoDB exitosa.")
-        db = client[SamplePatientService]
-        collection = db[patient]
+        db = client[db_name]
+        collection = db[collection_name]
         return collection
     except ConnectionFailure as e:
         print(f"Error de conexión a MongoDB: {e}")
